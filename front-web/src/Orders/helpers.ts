46e0101db5e9456551efe0172d0f6c3e727eb3d7
zaz <coderzaz@gmail.com>
2021-01-13 19:04:11 -0300

    Finish front-web

---
+++ front-web/src/Orders/helpers.ts
@@ -0,0 +1,8 @@
+import ProductCard from "./ProductCard";
+
+
+import { Product} from "./types";
+
+export function checkIsSelected(selectedProducts: Product[], product: Product){
+    return selectedProducts.some(item => item.id === product.id);
+}
